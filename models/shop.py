9f7bdb31236fbf1cbe5a1d1fb32de22f379e7717
VladmimirShtefan <shtefanvladimir14@gmail.com>
2022-12-17 19:32:44 +0700

    refactor

--- models/shop.py
+++ models/shop.py
@@ -9,10 +9,9 @@ class Shop(BaseStorage):
         super().__init__(items, capacity)
         self.__items = items
         self.__capacity = capacity
-        # self.__storage_name = 'магазин'
 
     def add(self, name: str, amount: int) -> None:
-        if self.__items.get(name) not in self.__items.values() and self.get_unique_items_count() == 5:
+        if self.__items.get(name) not in self.__items.values() and self.get_unique_items_count() >= 5:
             raise ErrorOverload(self.__storage_name__)
         super(Shop, self).add(name, amount)
 
